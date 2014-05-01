b0762427bf066e885aae1ba6ae562196ef014b02
Jack Zaharoff <jackzaharoff@gmail.com>
2014-05-01 18:43:49 +0000

    Change factories.rb to include more sensible username and email for user, add email regexp to devise.rb initializer.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
     @first_name = Faker::Name.first_name
-    sequence(:username) { |n| "#{@first_name}_#{n}"}
-    sequence(:email) { |n| "#{@first_name}_#{n}@example.com"}
+    sequence(:username, @first_name) { |n| "#{@first_name}_#{n}"}
+    sequence(:email, @first_name) { |n| "#{@first_name}_#{n}@example.com"}
     after(:build) { |u| u.password = u.password_confirmation = '123456789'}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe User do
   let(:user) { FactoryGirl.build(:user)}
   subject { user }
+  it { should be_valid }
   it { should respond_to(:email)}
   it { should respond_to(:username)}
   it { should validate_presence_of(:username)}
