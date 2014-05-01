b4e6fc20b22d20f086b5ea329ba3d753e6c66211
Jack Zaharoff <jackzaharoff@gmail.com>
2014-05-01 18:01:38 +0000

    Add tests for username, change options for lockable in devise initializer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable, :lockable #TODO add :omniauthable, :timeoutable
 
-  validates :username, presence: true, uniqueness: true
+  validates :username,  presence: true, uniqueness: true,
+                        format: { with: /[a-zA-Z0-9\-_]+/i},
+                        length: { minimum: 3, maximum: 40 }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,13 @@ describe User do
   it { should respond_to(:username)}
   it { should validate_presence_of(:username)}
   it { should validate_uniqueness_of(:username)}
+  describe 'when username is too short' do
+    before { user.username = 'aa'}
+    it { expect(user).not_to be_valid}
+  end
+  describe 'when username is too long' do
+    before { user.username = 'a'*41 }
+    it { expect(user).not_to be_valid}
+  end
 
 end
