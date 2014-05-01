689d116cd2d4b471065337485ba67488e3a534da
Jack Zaharoff <jackzaharoff@gmail.com>
2014-05-01 17:19:02 +0000

    Add lockable to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :validatable, :confirmable, :lockable #, :omniauthable, :timeoutable
+         :validatable, :confirmable, :lockable #TODO add :omniauthable, :timeoutable
+
   validates :username, presence: true, uniqueness: true
 
 end
